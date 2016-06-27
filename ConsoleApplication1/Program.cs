9c8ad5d9281ebe5dd57dd9b87172ace4043e2caf
Drew Williams <drewwilliams1982@googlemail.com>
2016-06-27 10:31:07 +0100

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Transactions;
+using IdentityModel.Client;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = GetClientToken();
+            CallApi(t);
+        }
+
+        static TokenResponse GetClientToken()
+        {
+            var client = new TokenClient(
+                "http://new/main/sso/idp/connect/token",
+                "silicon",
+                "F621F470-9731-4A25-80EF-67A6F7C5F4B8");
+            
+            return client.RequestClientCredentialsAsync("isamsscope").Result;
+        }
+
+        static void CallApi(TokenResponse response)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken(response.AccessToken);
+
+            var req = client.GetStringAsync("http://new/Main/newapi/private/framework/prereqstatus");
+
+            Console.WriteLine(req.Result);
+        }
+    }
+    
+}
